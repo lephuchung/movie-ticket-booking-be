037ba1ff65d123bb66826382979f9833498d03ea
thanhhuong <thanhhuong03012003@gmail.com>
2024-12-26 23:01:36 +0700

    chore!: remove redundant payment word in payment routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 
 // API thanh toán
-router.post('/payment', paymentController.processPayment);
+router.post('/', paymentController.processPayment);
 
 // API lấy thông tin thanh toán theo ID
-router.get('/payment/:id', paymentController.getPaymentById);
+router.get('/:id', paymentController.getPaymentById);
 
 // API cập nhật trạng thái thanh toán
-router.put('/payment/:id', paymentController.updatePaymentStatus);
+router.put('/:id', paymentController.updatePaymentStatus);
 
 module.exports = router;
